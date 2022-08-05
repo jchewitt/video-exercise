b0f9f7cf0379c8cfb41792f6afbdc58f2c861f08
Jon Hewitt <jchewitt@users.noreply.github.com>
2022-08-05 18:46:32 -0500

    fix

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
+console.log('Starting server...');
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
 app.listen(port);
